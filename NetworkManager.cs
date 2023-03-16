501a45b5a2aa9dfd67ae45f45de2a794703f87ac
nover <109939195+novervr@users.noreply.github.com>
2023-03-15 21:56:43 -0500

    Update NetworkManager.cs

--- NetworkManager.cs
+++ NetworkManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 
-public class NetworkManger : MonoBehaviourPunCallbacks
+public class NetworkManager : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
     void Start()
