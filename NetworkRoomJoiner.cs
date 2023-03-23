04cf811244bd1a2cabd026500fa21c22ae41fb31
nover <109939195+novervr@users.noreply.github.com>
2023-03-23 16:46:26 -0500

    Update NetworkRoomJoiner.cs

--- NetworkRoomJoiner.cs
+++ NetworkRoomJoiner.cs
@@ -6,7 +6,7 @@ using Photon.Realtime;
 
 namespace Photon.UnityXRPlayer
 
-public class NetworkManager : MonoBehaviourPunCallbacks
+public class NetworkRoomJoiner : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
     void Start()
