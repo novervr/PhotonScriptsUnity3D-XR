ae57803b8d67c6c780da87f63633ab0193072dc0
nover <109939195+novervr@users.noreply.github.com>
2023-04-02 16:12:58 -0500

    wip
    wip rn

---
+++ Showroom.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Pun;
+
+public class Showroom : MonoBehaviour
+    {
+    
+        public Text classicText;
+        public TMP_Text TMPText;
+        
+        
+        void Start();
+        
+        void OnTriggerEnter(Collider other)
+         if 
